f5e237738df2d9992a01d88ec5de0efe6d8d56ec
Meron Kebede <meronkebede@Merons-MacBook-Pro.local>
2020-07-08 11:41:37 -0400

    store page, server side validation

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublictKey = process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripeSecretKey, stripePublictKey)
+
 const express = require ('express')
 const app = express ()
+const fs = require ('fs')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.get('/store', function (req, res) {
+
+
+})
+
 app.listen(3000)
+
